d9bdf71b805cb11d322abf25d46f471b537f6272
Kajal Singhal <kajal.singhal@ultragenicglobal.com>
2021-10-15 00:01:22 +0530

    todo list

---
+++ index.js
@@ -0,0 +1,53 @@
+var myNodeList = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodeList.length; i++) {
+    var span = document.createElement("SPAN");
+    var text = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(text);
+    myNodeList[i].appendChild(span);
+}
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
+}, false);
+
+function addTask() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myinput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+        alert("you must write something!");
+    } else {
+        document.getElementById('list').appendChild(li);
+    }
+
+    document.getElementById("myinput").value = "";
+
+    var span = document.createElement("SPAN");
+    var text = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(text);
+    li.appendChild(span);
+    // document.getElementsByClassName('close').style.top += 36 + 'px';
+
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+}
